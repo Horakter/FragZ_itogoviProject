0ffffd60114d27d10cef427ced05ccc7a8688d55
DIma <DIma@DIMKA>
2024-12-24 02:03:06 +0300

    Добавьте файлы проекта.

---
+++ FragrantWorld/Auentification.cs
@@ -0,0 +1,60 @@
+using FragrantWorld.Interface;
+using FragrantWorld.Services;
+
+namespace FragrantWorld
+{
+
+    public partial class FW_Autorization : Form
+    {
+
+        private string userRole;
+        public FW_Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Sign_Click(object sender, EventArgs e)
+        {
+            SignForm sign = new();
+            sign.Show();
+            this.Hide();
+
+        }
+
+        private void FW_Autorization_Load(object sender, EventArgs e)
+        {
+            Sign.BackColor = Color.FromArgb(204, 102, 0);
+            Registration.BackColor = Color.FromArgb(204, 102, 0);
+            SignInGuest.LinkColor = Color.FromArgb(204, 102, 0);
+        }
+
+        private void Registration_Click(object sender, EventArgs e)
+        {
+            RegistrationForm registration = new();
+            registration.Show();
+            this.Hide();
+        }
+
+        private void SignInGuest_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FragrantWorldShop shop = new FragrantWorldShop("Guest", null);
+            shop.Show();
+            this.Hide();
+
+        }
+
+        private void FW_Autorization_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены что хотите покинуть приложение?", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ FragrantWorld/SignForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace FragrantWorld
+{
+    partial class SignForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignForm));
+            FWSign_tableLayoutPanel = new TableLayoutPanel();
+            backButton = new Button();
+            sign_LoginLbl = new Label();
+            sign_LoginTextBox = new TextBox();
+            sign_PasswordLbl = new Label();
+            sign_PasswordTextBox = new TextBox();
+            sign_ForgotLinkLbl = new LinkLabel();
+            signButton = new Button();
+            FWSign_tableLayoutPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // FWSign_tableLayoutPanel
+            // 
+            FWSign_tableLayoutPanel.BackColor = Color.White;
+            FWSign_tableLayoutPanel.ColumnCount = 3;
+            FWSign_tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 39.66F));
+            FWSign_tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.34F));
+            FWSign_tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 27F));
+            FWSign_tableLayoutPanel.Controls.Add(backButton, 0, 0);
+            FWSign_tableLayoutPanel.Controls.Add(sign_LoginLbl, 1, 1);
+            FWSign_tableLayoutPanel.Controls.Add(sign_LoginTextBox, 1, 2);
+            FWSign_tableLayoutPanel.Controls.Add(sign_PasswordLbl, 1, 3);
+            FWSign_tableLayoutPanel.Controls.Add(sign_PasswordTextBox, 1, 4);
+            FWSign_tableLayoutPanel.Controls.Add(sign_ForgotLinkLbl, 1, 5);
+            FWSign_tableLayoutPanel.Controls.Add(signButton, 1, 6);
+            FWSign_tableLayoutPanel.Dock = DockStyle.Fill;
+            FWSign_tableLayoutPanel.Font = new Font("Comic Sans MS", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FWSign_tableLayoutPanel.GrowStyle = TableLayoutPanelGrowStyle.AddColumns;
+            FWSign_tableLayoutPanel.Location = new Point(0, 0);
+            FWSign_tableLayoutPanel.Margin = new Padding(3, 2, 3, 2);
+            FWSign_tableLayoutPanel.Name = "FWSign_tableLayoutPanel";
+            FWSign_tableLayoutPanel.RowCount = 8;
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 22F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 29F));
+            FWSign_tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            FWSign_tableLayoutPanel.Size = new Size(700, 338);
+            FWSign_tableLayoutPanel.TabIndex = 0;
+            FWSign_tableLayoutPanel.Paint += tableLayoutPanel1_Paint;
+            // 
+            // backButton
+            // 
+            backButton.BackColor = Color.FromArgb(204, 102, 0);
+            backButton.Location = new Point(3, 2);
+            backButton.Margin = new Padding(3, 2, 3, 2);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(66, 22);
+            backButton.TabIndex = 1;
+            backButton.Text = "Назад";
+            backButton.UseVisualStyleBackColor = false;
+            backButton.Click += backButton_Click;
+            // 
+            // sign_LoginLbl
+            // 
+            sign_LoginLbl.AutoSize = true;
+            sign_LoginLbl.Location = new Point(280, 99);
+            sign_LoginLbl.Name = "sign_LoginLbl";
+            sign_LoginLbl.Size = new Size(49, 17);
+            sign_LoginLbl.TabIndex = 0;
+            sign_LoginLbl.Text = "Логин:";
+            // 
+            // sign_LoginTextBox
+            // 
+            sign_LoginTextBox.BackColor = Color.FromArgb(204, 102, 0);
+            sign_LoginTextBox.Location = new Point(280, 123);
+            sign_LoginTextBox.Margin = new Padding(3, 2, 3, 2);
+            sign_LoginTextBox.MaxLength = 32;
+            sign_LoginTextBox.Name = "sign_LoginTextBox";
+            sign_LoginTextBox.Size = new Size(172, 24);
+            sign_LoginTextBox.TabIndex = 1;
+            // 
+            // sign_PasswordLbl
+            // 
+            sign_PasswordLbl.AutoSize = true;
+            sign_PasswordLbl.Location = new Point(280, 143);
+            sign_PasswordLbl.Name = "sign_PasswordLbl";
+            sign_PasswordLbl.Size = new Size(56, 17);
+            sign_PasswordLbl.TabIndex = 2;
+            sign_PasswordLbl.Text = "Пароль:";
+            // 
+            // sign_PasswordTextBox
+            // 
+            sign_PasswordTextBox.BackColor = Color.FromArgb(204, 102, 0);
+            sign_PasswordTextBox.Location = new Point(280, 167);
+            sign_PasswordTextBox.Margin = new Padding(3, 2, 3, 2);
+            sign_PasswordTextBox.Name = "sign_PasswordTextBox";
+            sign_PasswordTextBox.Size = new Size(172, 24);
+            sign_PasswordTextBox.TabIndex = 3;
+            // 
+            // sign_ForgotLinkLbl
+            // 
+            sign_ForgotLinkLbl.ActiveLinkColor = Color.RosyBrown;
+            sign_ForgotLinkLbl.AutoSize = true;
+            sign_ForgotLinkLbl.BackColor = Color.White;
+            sign_ForgotLinkLbl.DisabledLinkColor = Color.White;
+            sign_ForgotLinkLbl.LinkBehavior = LinkBehavior.NeverUnderline;
+            sign_ForgotLinkLbl.LinkColor = Color.FromArgb(204, 102, 0);
+            sign_ForgotLinkLbl.Location = new Point(280, 187);
+            sign_ForgotLinkLbl.Name = "sign_ForgotLinkLbl";
+            sign_ForgotLinkLbl.Size = new Size(103, 17);
+            sign_ForgotLinkLbl.TabIndex = 4;
+            sign_ForgotLinkLbl.TabStop = true;
+            sign_ForgotLinkLbl.Text = "Забыли пароль?";
+            // 
+            // signButton
+            // 
+            signButton.BackColor = Color.FromArgb(204, 102, 0);
+            signButton.Location = new Point(280, 211);
+            signButton.Margin = new Padding(3, 2, 3, 2);
+            signButton.Name = "signButton";
+            signButton.Size = new Size(172, 25);
+            signButton.TabIndex = 5;
+            signButton.Text = "Войти";
+            signButton.UseVisualStyleBackColor = false;
+            signButton.Click += signButton_Click;
+            // 
+            // SignForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(FWSign_tableLayoutPanel);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "SignForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sign";
+            FormClosing += SignForm_FormClosing;
+            FWSign_tableLayoutPanel.ResumeLayout(false);
+            FWSign_tableLayoutPanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel FWSign_tableLayoutPanel;
+        private Label sign_LoginLbl;
+        private TextBox sign_LoginTextBox;
+        private TextBox sign_PasswordTextBox;
+        private Label sign_PasswordLbl;
+        private LinkLabel sign_ForgotLinkLbl;
+        private Button signButton;
+
+        private Button backButton;
+    }
+}
